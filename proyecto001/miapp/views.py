b8986b9e2ca4579c97f33f7ea71d69a7b9bb33ce
Flor Cerdan <cerdanflor@gmail.com>
2022-12-21 10:34:36 -0500

    DJango Parte 8a Lenguaje de Plantillas y templates

--- proyecto001/miapp/views.py
+++ proyecto001/miapp/views.py
@@ -24,24 +24,35 @@ layout = """
 
 
 def index(request):
-    return render(request , 'index.html')
+    estudiantes = [
+                    'Jhon Baldeon',
+                    'Alexander Gonzales',
+                    'Joan Palomino',
+                    'Carlos Aquino',
+                    'Danne Barzola']
+
+    return render(request , 'index.html',{
+        'titulo': 'Inicio',
+        'mensaje':'Proyecto Web con Django',
+        'estudiantes': estudiantes
+    })
 
 def saludo(request):
-    return render(request, 'saludo.html')
+    return render(request, 'saludo.html',{
+        'titulo':'Saludo',
+        'autor_saludo':'Mg. Flor Elizabeth Cerdán León'
+    })
 
 def rango(request) :
         a = 10
         b = 20
-        resultado = f"""
-            <h2> Numeros de [{a},{b}] </h2>
-            Resultado:<br>
-            <ul>
-        """
-        while a<=b:
-            resultado += f"<li> {a} </li>"
-            a+=1
-        resultado += "</ul>"
-        return HttpResponse(layout + resultado)
+        rango_numeros = range(a,b+1)        
+        return render(request, 'rango.html',{
+            'titulo': 'Rango',
+            'a':a,
+            'b':b,
+            'rango_numeros':rango_numeros
+        })
 
 def rango2(request,a=0,b=100) :
 
